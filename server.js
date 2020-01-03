b287f6e5a59ab69fedb7641f0fb9e554ffbdf55e
A01650434 <47993558+A01650434@users.noreply.github.com>
2020-01-03 00:12:29 -0600

    register/login/hello/working

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
+//load in environment var and set the inside process.env
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcryptjs')
-//const passport = require('passport')
+const passport = require('passport')
+const flash = require('express-flash')
+const session =require('express-session')
 
 //require passport function
-//const initializePassport = require('./passport-config')
-//initializePassport(passport)
+const initializePassport = require('./passport-config')
+initializePassport(passport, 
+    email => users.find(user  => user.email === email),
+    id => users.find(user  => user.id === id)
+    )
 //for  storing locally nodb (at every refresh it's gonna be deleted)
 const users = []
 //Pointing its using .ejs
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))//taking formas and bulding access
+//server to use passport
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false 
+}))
+app.use(passport.initialize())//func inside pass to set the basics
+app.use(passport.session())//to make var persited during the cross of the entire session
 
 //set the route for application
 app.get('/', (req, res) => {
@@ -22,9 +41,11 @@ app.get('/login', (req, res) => { //verify user
     res.render('login.ejs')
 })
 
-app.post('/login', (req,  res) => {
-
-})
+app.post('/login', passport.authenticate('local',{
+   successRedirect: '/', //where to go when success
+   failureRedirect: '/login',
+   failureFlash: true
+}))
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
